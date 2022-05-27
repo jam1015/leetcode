e0b56c048da77ca0f0db2f17f7cf164b7e496d0f
Jordan Mandel <jordan.mandel@live.com>
2022-05-27 15:12:44 -0400

    possible queue based solution

--- python/duplicate_zeros/duplicate_zeros_queue.py
+++ python/duplicate_zeros/duplicate_zeros_queue.py
@@ -1,14 +1,11 @@
+from collections import deque
 ra = [0, 1, 2, 3, 4, 0, 5, 6, 7, 8, 9, 10]
+q = deque()
 
-def dup_element(ind_in, arr_in):
-    print(f"array in: {arr_in}")
-    lenth = len(arr_in)
-    for k in reversed(range(ind_in, lenth - 1 )):
-        arr_in[k + 1] = arr_in[k]
-        print(k)
-    print(f"            array out: {arr_in}")
-
-
-for k in reversed(range(len(ra))):
+for k in range(len(ra)):
+    q.append(ra[k])
     if ra[k] == 0:
-        dup_element(k, ra)
+        q.append(ra[k])
+    ra[k] = q.popleft()
+
+print(ra)
